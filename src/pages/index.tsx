966ba8e772d639af656e5b8fde003f163f16e8c8
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-05-26 14:54:15 -0300

    :sparkles: Added price from stripe products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
+import { GetServerSideProps } from 'next';
 import Head from "next/head"
 import Image from "next/image"
+
+import { stripe } from '../services/stripe';
+
 import { SubscribeButton } from "../components/SubscribeButton"
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -20,11 +31,11 @@ export default function Home() {
           <p>
             Get access to all the publications <br />
             <span>
-              For $9,90 month
+              For {product.amount} month
             </span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image
@@ -36,4 +47,22 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1L3UEiI2hyn9AKBDWsWltzTo');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
